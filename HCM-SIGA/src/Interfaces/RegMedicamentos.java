0d5b729140a1070924b33d36e489e65e0acc7fa2
diiogo91 <diogoamaral91@gmail.com>
2014-07-12 18:53:15 +0200

    Registro

--- HCM-SIGA/src/Interfaces/RegMedicamentos.java
+++ HCM-SIGA/src/Interfaces/RegMedicamentos.java
@@ -10,6 +10,10 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import java.awt.Font;
 
 public class RegMedicamentos extends JFrame {
 
@@ -36,20 +40,36 @@ public class RegMedicamentos extends JFrame {
 	 */
 	public RegMedicamentos() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 572, 420);
+		setBounds(100, 100, 698, 574);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(205, 133, 63));
+		panel.setBackground(new Color(70, 130, 180));
 		contentPane.add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(33, 37, 478, 277);
+		panel_1.setBackground(new Color(255, 255, 255));
+		panel_1.setBounds(29, 49, 614, 428);
 		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(315, 285, 289, 121);
+		panel_1.add(scrollPane);
+		
+		JTextPane txtpnTest = new JTextPane();
+		txtpnTest.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		txtpnTest.setText("test");
+		scrollPane.setViewportView(txtpnTest);
+		
+		JLabel lblC = new JLabel("Comentario do Medico:");
+		lblC.setFont(new Font("Consolas", Font.PLAIN, 20));
+		lblC.setBounds(30, 328, 231, 24);
+		panel_1.add(lblC);
 	}
 	private Point findScreenCenter() {
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
