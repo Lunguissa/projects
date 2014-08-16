5008710140a0938e043275a4693b6da50507eb59
TRosario619 <tassiorosario@gmail.com>
2014-08-16 03:11:49 +0200

    bean em falta

---
+++ HCM-SIGA/src/mz/co/lunguissa/entity/Idade.java
@@ -0,0 +1,35 @@
+package mz.co.lunguissa.entity;
+
+public class Idade 
+{
+	private int codigoIdade;
+	private String descricao;
+	private int minDias;
+	private int maxDias;
+	public int getCodigoIdade() {
+		return codigoIdade;
+	}
+	public void setCodigoIdade(int codigoIdade) {
+		this.codigoIdade = codigoIdade;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getMinDias() {
+		return minDias;
+	}
+	public void setMinDias(int minDias) {
+		this.minDias = minDias;
+	}
+	public int getMaxDias() {
+		return maxDias;
+	}
+	public void setMaxDias(int maxDias) {
+		this.maxDias = maxDias;
+	}
+	
+	
+}
